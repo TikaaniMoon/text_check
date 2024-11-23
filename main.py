b09638960a562b52a5a312933b2650e03b3874ab
Tikaani <dara.stormy@gmail.com>
2024-11-23 15:08:30 +0200

    feat: added a check for the purity of words

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+import re
+
 text = input('Введите текст: ')
 text = text.lower()
 
-punctuation = [".", ",", "!", "?"]
-for symb in punctuation:
-    text = text.replace(symb, '')
+# punctuation = ['.', ',', '!', '?']
+# for symb in punctuation:
+#     text = text.replace(symb, '')
+text = re.sub(r'[^\w\s]', '', text)
 
 words = text.split()
 
-print("Количество разных слов:", len(set(words)))
+word_frequency = {}
+
+print('Количество разных слов:', len(set(words)))
+print('Частота слов: ')
+
+for word in words:
+    if word in word_frequency:
+        word_frequency[word] += 1
+    else:
+        word_frequency[word] = 1
+
+for word, frequency in word_frequency.items():
+    print(f'{word}: {frequency}')
+
+
 
 
